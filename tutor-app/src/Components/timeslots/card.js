79abc85de609f540425b57b7b3483d23b44e0f5a
Felipe Sanchez <primetimeofyourlifehere@gmail.com>
2020-04-25 18:44:52 -0400

    BG color

--- tutor-app/src/Components/timeslots/card.js
+++ tutor-app/src/Components/timeslots/card.js
@@ -6,17 +6,14 @@ class Cards extends Component {
         return (
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
-                    <div className='col-xs-12 col-md-4 col-lg-2'><Card /></div>
-                    <div className='col-xs-12 col-md-4 col-lg-2'><Card /></div>
-                    <div className='col-xs-12 col-md-4 col-lg-2'><Card /></div>
-                    <div className='col-xs-12 col-md-4 col-lg-2'><Card /></div>
-                    <div className='col-xs-12 col-md-4 col-lg-2'><Card /></div>
+                    <div className='col-xs-12 col-md-4 col-lg-2'><h4>Monday</h4><Card /></div>
+                    <div className='col-xs-12 col-md-4 col-lg-2'><h4>Tuesday</h4><Card /></div>
+                    <div className='col-xs-12 col-md-4 col-lg-2'><h4>Wednesday</h4><Card /></div>
+                    <div className='col-xs-12 col-md-4 md-offset-2 col-lg-2'><h4>Thursday</h4><Card /></div>
+                    <div className='col-xs-12 col-md-4 col-lg-2'><h4>Friday</h4><Card /></div>
                 </div>
             </div>
         )
     }
-
-
-
 }
 export default Cards;
--- tutor-app/src/Components/timeslots/cardUI.js
+++ tutor-app/src/Components/timeslots/cardUI.js
@@ -5,9 +5,8 @@ import './card-style.css';
 const Card = props => {
     return (
         <div className="card-body tex-dark">
-            <h3 className="card-title">Monday</h3>
-            <h4 className="Card-text text-secondary">Pablo Picasso</h4>
-            <a href="#" className="btn btn-outline-success btn-tutor-session">Go to session</a>
+            <h3 className="card-title">TA's name</h3>
+            <h4 className="Card-text text-secondary">Classes</h4>
         </div>
     );
 
