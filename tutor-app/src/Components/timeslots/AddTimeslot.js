b7f2146ba41a304d0cc6a44a4313b9390bae367b
Benjamin Thaw <bthaw2018@fau.edu>
2020-04-26 05:27:36 -0400

    Added firebase functions, and connected functions to a form to add timeslots

--- tutor-app/src/Components/timeslots/AddTimeslot.js
+++ tutor-app/src/Components/timeslots/AddTimeslot.js
@@ -8,13 +8,25 @@ class CreateTimeslot extends React.Component {
     componentDidMount() {
         M.AutoInit();
     }
+    HandleFormSubmit(e) {
+        e.preventDefault();
+        var tutor_email = e.target.querySelector("#tutor_email").value;
+
+        var functions = firebase.functions();
+        var addTS = functions.httpsCallable('addTimeslot');
+        addTS({ tutor_email: tutor_email }).then(() => {
+            console.log("success!")
+        }).catch(err => {
+            console.log(err.message);
+        })
+    }
     render() {
         return (
             <div className="container">
                 <div className="createTimeslot">
                     <div className="row"></div>
-                    <form className="col s12">
-                        <div className="row">
+                    <form onSubmit={this.HandleFormSubmit} className="col s12">
+                        {/* <div className="row">
                             <div className="input-field col s12 m6">
                                 <input type="text" className="datepicker" id="start_date" name="start_date" required></input>
                                 <label for="start_date">Start Date</label>
@@ -33,7 +45,23 @@ class CreateTimeslot extends React.Component {
                                 <input type="text" className="timepicker" id="end_time" name="end_time" required></input>
                                 <label for="end_time">End Time</label>
                             </div>
+                        </div> */}
+                        <div className="row">
+                            <div class="input-field col s12">
+                                <input type="email" name="tutor_email" id="tutor_email" className="validate" required></input>
+                                <label for="tutor_email">Tutor Email</label>
+                            </div>
                         </div>
+                        {/* <div className="row">
+                            <div class="input-field col s12">
+                                <input type="email" name="student_email" id="student_email" className="validate" required></input>
+                                <label for="student_email">Student Email</label>
+                            </div>
+                        </div> */}
+                        <div className="row">
+                            <button type="submit" className="btn waves-effect waves-light">Add Timeslot</button>
+                        </div>
+
 
 
 
